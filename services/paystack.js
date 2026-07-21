8775f686102d70ae9cb44e3d313ae85c41b4c3d9
onebetoneclick <onebet.oneclick@gmail.com>
2026-07-21 13:34:31 +0100

    Create paystack.js

---
+++ services/paystack.js
@@ -0,0 +1,23 @@
+const axios = require("axios");
+
+
+const paystack = axios.create({
+
+    baseURL:
+    "https://api.paystack.co",
+
+
+    headers:{
+
+        Authorization:
+        `Bearer ${process.env.PAYSTACK_SECRET_KEY}`,
+
+        "Content-Type":
+        "application/json"
+
+    }
+
+});
+
+
+module.exports = paystack;
