9d69fdc3f5ffb3cad3a86bda429bc066c38e5bf1
onebetoneclick <onebet.oneclick@gmail.com>
2026-07-22 12:11:46 +0100

    Update bank.js

--- routes/bank.js
+++ routes/bank.js
@@ -2,14 +2,15 @@ const express = require("express");
 const router = express.Router();
 
 const {
+    getBanks,
     verifyBankAccount,
     createTransferRecipient
 } = require("../controllers/bankController");
 
+router.get("/list", getBanks);
+
 router.post("/verify", verifyBankAccount);
 
 router.post("/recipient", createTransferRecipient);
-
-router.get("/list", getBanks);
 
 module.exports = router;
