7230bcd7d17d4c74ab779382dbeec72e61048e85
onebetoneclick <onebet.oneclick@gmail.com>
2026-07-22 11:59:36 +0100

    Update withdrawal.js

--- routes/withdrawal.js
+++ routes/withdrawal.js
@@ -1,101 +1,10 @@
 const express = require("express");
-
 const router = express.Router();
 
-const paystack = require("../services/paystack");
-
-
-
-router.post("/", async(req,res)=>{
-
-
-try{
-
-
 const {
+    withdrawMoney
+} = require("../controllers/withdrawalController");
 
-amount,
-account_number,
-bank_code,
-name
-
-}=req.body;
-
-
-
-// create transfer recipient
-
-const recipient =
-await paystack.post(
-"/transferrecipient",
-{
-
-type:"nuban",
-
-name:name,
-
-account_number,
-
-bank_code,
-
-currency:"NGN"
-
-}
-
-);
-
-
-
-const transfer =
-await paystack.post(
-"/transfer",
-{
-
-
-source:"balance",
-
-amount: amount * 100,
-
-
-recipient:
-recipient.data.data.recipient_code,
-
-
-reason:
-"Wallet withdrawal"
-
-
-});
-
-
-
-res.json(transfer.data);
-
-
-
-}
-
-catch(error){
-
-
-res.status(500).json({
-
-message:
-"Withdrawal failed",
-
-error:
-error.response?.data ||
-error.message
-
-});
-
-
-}
-
-
-
-});
-
-
+router.post("/", withdrawMoney);
 
 module.exports = router;
